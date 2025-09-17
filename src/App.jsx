7d28fb3c84e8ed0843faea96110b9a4b47d3b5e9
spl-NicoValderrama <valderrama.nicolas@outlook.com>
2025-09-17 10:44:17 -0300

    fix App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p className="read-the-docs">Inicialización del repositorio y branches</p>
+    </>
+  );
+}
+
+export default App;
